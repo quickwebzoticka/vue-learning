7e84b093b85c101d368bfc81349c766e511716e0
Quickzotick <solk010@gmail.com>
2019-07-13 21:08:01 +0300

    VUE CLI initialized

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello world 2!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-project/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App1 from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App1)
+})
