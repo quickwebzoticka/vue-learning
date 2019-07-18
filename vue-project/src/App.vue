6ffb4457da703ffb09a5391c92c916310c100e44
Quickzotick <solk010@gmail.com>
2019-07-18 23:29:17 +0300

    block 7 completed about forms

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -9,6 +9,75 @@
     <hr>
 
     <app-list></app-list>
+
+    <hr>
+
+    <textarea v-model="textarea"></textarea>
+    <p>{{ textarea }}</p>
+
+    <hr>
+
+    <div>
+      <label>
+        <input type="checkbox" value="vk" v-model="social">
+        <span>VK</span>
+      </label>
+      <label>
+        <input type="checkbox" value="instagram" v-model="social">
+        <span>Instagram</span>
+      </label>
+      <label>
+        <input type="checkbox" value="tiktok" v-model="social">
+        <span>tiktok</span>
+      </label>
+
+      <div>
+        <ul>
+          <li v-for="s of social">{{ s }}</li>
+        </ul>
+      </div>
+    </div>
+
+    <div>
+      <label>
+        <input type="radio" value="vk" v-model="socialRadio">
+        <span>VK</span>
+      </label>
+      <label>
+        <input type="radio" value="instagram" v-model="socialRadio">
+        <span>Instagram</span>
+      </label>
+      <label>
+        <input type="radio" value="tiktok" v-model="socialRadio">
+        <span>tiktok</span>
+      </label>
+
+      <div>
+        {{ socialRadio }}
+      </div>
+    </div>
+
+    <div>
+      <select v-model="socialRadio">
+        <option
+          :key="s"
+          v-for="s of socialNets"
+        >{{ s }}</option>
+      </select>
+    </div>
+
+    <div>
+      <input type="text" v-model.number="age"> {{ age }}
+    </div>
+
+    <hr>
+
+    <div>
+      <app-onoff v-model="switched"></app-onoff>
+
+      <div v-if="switched">Component enabled</div>
+      <div v-else>Cmpoent disabled</div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +86,7 @@
 import Car from './Car.vue'
 import Counter from './Counter.vue'
 import listMixin from './listMixin.js'
+import appOnOff from './appOnOff'
 
 
 export default {
@@ -25,10 +95,32 @@ export default {
     lowercase(value) {
       return value.toLowerCase()
     }
+  },
+  data() {
+    return {
+      age: 25,
+      textarea: 'Initial text',
+      social: ['vk'],
+      socialRadio: 'vk',
+      socialNets: ['vk', 'instagram', 'tiktok'],
+      switched: true
+    }
+  },
+  components: {
+    'app-onoff': appOnOff
   }
 }
 </script>
 
 <style scoped>
-  
+  p {
+    white-space: pre;
+  }
+  textarea {
+    width: 400px;
+
+    height: 100px;
+  }
+
+
 </style>
---
+++ vue-project/src/appOnOff.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="button"
+            :class="{'on': value}"    
+            @click="switched(true)"
+        >On</div>
+        <div class="button"
+            :class="{'off': !value}"
+            @click="switched(false)"
+        >Off</div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: ['value'],
+    methods: {
+        switched(newVal) {
+            this.$emit('input', newVal)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.button{
+    padding: 20px;
+    background: #999;
+}
+
+.button.on{
+    background: green;
+}
+
+.button.off{
+    background: red;
+}
+</style>
+
